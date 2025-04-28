43297ad81fa096ff9c476ebac3cf54b9fc45e1ec
jayx20p <jaydenp170@gmail.com>
2025-04-29 00:04:54 +0800

    App.jsx fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { LoadScript } from "@react-google-maps/api";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import AddBooking from "./pages/AddBooking";
 import EditBooking from "./pages/EditBooking";
-import Login from "./pages/Login";
-import ErrorPage from "./pages/ErrorPage";
-import { BookingContext } from "./contexts/BookingContext";
 import useLocalStorage from "use-local-storage";
+import { BookingContext } from "./contexts/BookingContext";
+import { LoadScript } from "@react-google-maps/api";
+import { useState } from "react";
+
+// Layout WITH Navbar (used for home, add booking, etc.)
+function Layout() {
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/home">Appointments</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">Add Appointment</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+}
 
 export default function App() {
   const [bookings, setBookings] = useLocalStorage("bookings", []);
+  const [mapsLoaded, setMapsLoaded] = useState(false);
+
+  console.log("Google Maps API Key:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
 
   return (
     <LoadScript
       googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
       libraries={["places"]}
+      onLoad={() => setMapsLoaded(true)}
     >
-      <BookingContext.Provider value={{ bookings, setBookings }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/add" element={<AddBooking />} />
-            <Route path="/edit/:bookingId" element={<EditBooking />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </BrowserRouter>
-      </BookingContext.Provider>
+      {mapsLoaded ? (
+        <BookingContext.Provider value={{ bookings, setBookings }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route element={<Layout />}>
+                <Route path="/home" element={<Home />} />
+                <Route path="/add" element={<AddBooking />} />
+                <Route path="/edit/:bookingId" element={<EditBooking />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </BookingContext.Provider>
+      ) : (
+        <div style={{ textAlign: "center", marginTop: "100px" }}>
+          <h5>Loading Google Maps...</h5>
+        </div>
+      )}
     </LoadScript>
   );
 }

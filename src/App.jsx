e93d322fa99e6706a5ec3c66507e7d45086be0d3
jayx20p <jaydenp170@gmail.com>
2025-04-28 23:43:01 +0800

    added console.log for API key

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,9 @@ function Layout() {
 export default function App() {
   const [bookings, setBookings] = useLocalStorage("bookings", []);
 
+  console.log("Google Maps API Key:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
+
+
   return (
     <LoadScript
       googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}

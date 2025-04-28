b851aabc52fe8fdd47bd1dcd577e3afb233eda00
jayx20p <jaydenp170@gmail.com>
2025-04-28 23:59:09 +0800

    App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import ErrorPage from "./pages/ErrorPage";
+import { LoadScript } from "@react-google-maps/api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import AddBooking from "./pages/AddBooking";
 import EditBooking from "./pages/EditBooking";
-import useLocalStorage from "use-local-storage";
+import Login from "./pages/Login";
+import ErrorPage from "./pages/ErrorPage";
 import { BookingContext } from "./contexts/BookingContext";
-import { LoadScript } from "@react-google-maps/api";
-
-// Layout WITH Navbar (used for home, add booking, etc.)
-function Layout() {
-  return (
-    <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="/home">Appointments</Navbar.Brand>
-          <Nav>
-            <Nav.Link href="/add">Add Appointment</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Outlet />
-    </>
-  );
-}
+import useLocalStorage from "use-local-storage";
 
 export default function App() {
   const [bookings, setBookings] = useLocalStorage("bookings", []);
-
-  console.log("Google Maps API Key:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
-
 
   return (
     <LoadScript
@@ -42,12 +21,10 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
-            <Route element={<Layout />}>
-              <Route path="/home" element={<Home />} />
-              <Route path="/add" element={<AddBooking />} />
-              <Route path="/edit/:bookingId" element={<EditBooking />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Route>
+            <Route path="/home" element={<Home />} />
+            <Route path="/add" element={<AddBooking />} />
+            <Route path="/edit/:bookingId" element={<EditBooking />} />
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </BrowserRouter>
       </BookingContext.Provider>
